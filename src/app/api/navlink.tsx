25a009777f80c38746e85adaa52729f82b230aa7
Jonathan Sivahera <sivaheradev@gmail.com>
2026-01-25 03:44:40 +0200

    Removed old configuration

--- src/app/api/navlink.tsx
+++
@@ -1,9 +0,0 @@
-import { NavLinks } from '@/types/navlink'
-
-export const navLinks: NavLinks[] = [
-  { label: 'home', href: '/' },
-  { label: 'projects', href: '/projects' },
-  { label: 'blog', href: '/blog' },
-  { label: 'contact', href: '/contact-us' },
-  { label: 'docs', href: '/documentation' },
-]
